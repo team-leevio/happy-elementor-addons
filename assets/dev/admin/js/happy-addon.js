c0a73d01fd012bba25b5d751ba20b1ec6d1612e3
obiPlabon <obi.plabon@gmail.com>
2019-05-15 20:31:44 +0600

    Fixed search panel widget box gradient missing

--- assets/dev/admin/js/happy-addon.js
+++ assets/dev/admin/js/happy-addon.js
@@ -1,18 +1,12 @@
 ;(function($, elementor) {
     'use strict';
 
-    function initHappyAddonsAddon( $wrapper ) {
-        $wrapper
-            .find('.hm')
-            .parents('.elementor-element')
-            .addClass('happy-addons-addon');
-    }
-
     elementor.on('panel:init', function() {
-        initHappyAddonsAddon($('#elementor-panel-category-happy_addons'));
-
         $('#elementor-panel-elements-search-input').on('keyup', _.debounce(function() {
-            initHappyAddonsAddon($('#elementor-panel-elements'));
+            $('#elementor-panel-elements')
+                .find('.hm')
+                .parents('.elementor-element')
+                .addClass('happy-addons-addon');
         }, 170));
     });
 }(jQuery, window.elementor));
