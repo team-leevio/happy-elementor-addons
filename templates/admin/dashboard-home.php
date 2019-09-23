aea5bf998ce92e03f649fda26f098aadeb15177b
Mosaddek <Mosaddek73@gmail>
2019-09-23 17:12:45 +0600

    dashboard home done

--- templates/admin/dashboard-home.php
+++ templates/admin/dashboard-home.php
@@ -13,66 +13,156 @@ defined( 'ABSPATH' ) || die();
         </div>
     </div>
     <div class="ha-home-body">
-        <div class="ha-row">
+        <div class="ha-row ha-py-5 ha-align-items-center">
             <div class="ha-col ha-col-6">
-                <h3>Knowledge & Wiki</h3>
-                <p>We have created full-proof documentation for you. It will help you to understand how our plugin works.</p>
-                <a target="_blank" rel="noopener" href="">Take Me to The Knowledge Page</a>
+                <img class="ha-img-fluid ha-title-icon-size" src="<?php echo HAPPY_ADDONS_ASSETS; ?>imgs/admin/knowledge.svg" alt="">
+                <h3 class="ha-feature-title">Knowledge & Wiki</h3>
+                <p class="f18">We have created full-proof documentation for you. It will help you to understand how our plugin works.</p>
+                <a class="ha-btn ha-btn-primary" target="_blank" rel="noopener" href="">Take Me to The Knowledge Page</a>
             </div>
             <div class="ha-col ha-col-6">
-                <h3>Knowledge & Wiki</h3>
-                <p>We have created full-proof documentation for you. It will help you to understand how our plugin works.</p>
-                <a target="_blank" rel="noopener" href="">Take Me to The Knowledge Page</a>
+                <img class="ha-img-fluid" src="<?php echo HAPPY_ADDONS_ASSETS; ?>imgs/admin/art1.png" alt="">
+            </div>
+        </div>
+        <div class="ha-row ha-py-5 ha-pt-0">
+            <div class="ha-col ha-col-12">
+                <img class="ha-img-fluid ha-title-icon-size" src="<?php echo HAPPY_ADDONS_ASSETS; ?>imgs/admin/film.svg" alt="">
+                <h3 class="ha-feature-title">Video Tutorial</h3>
+                <p class="f16">How to use Floating Effects and manage CSS Transform?</p>
+            </div>
+            <div class="ha-col ha-col-4">
+                <img class="ha-img-fluid ha-rounded" src="<?php echo HAPPY_ADDONS_ASSETS; ?>imgs/admin/v-thumb.jpg" alt="">
+                <h4 class="ha-feature-sub-title"><a href="#">How to create beautiful timeline with happyaddons in elementor</a></h4>
+            </div>
+            <div class="ha-col ha-col-4">
+                <img class="ha-img-fluid ha-rounded" src="<?php echo HAPPY_ADDONS_ASSETS; ?>imgs/admin/v-thumb.jpg" alt="">
+                <h4 class="ha-feature-sub-title"><a href="#">How to create beautiful timeline with happyaddons in elementor</a></h4>
+            </div>
+            <div class="ha-col ha-col-4">
+                <img class="ha-img-fluid ha-rounded" src="<?php echo HAPPY_ADDONS_ASSETS; ?>imgs/admin/v-thumb.jpg" alt="">
+                <h4 class="ha-feature-sub-title"><a href="#">How to create beautiful timeline with happyaddons in elementor</a></h4>
+            </div>
+            <div class="ha-col ha-col-12 ha-align-center ha-pt-2">
+                <a class="ha-btn ha-btn-secondary" target="_blank" rel="noopener" href="">View more videos</a>
+            </div>
+        </div>
+        <div class="ha-row ha-align-items-end ha-py-5 ha-pt-0">
+            <div class="ha-col ha-col-9">
+                <img class="ha-img-fluid ha-title-icon-size" src="<?php echo HAPPY_ADDONS_ASSETS; ?>imgs/admin/faq.svg" alt="">
+                <h3 class="ha-feature-title ha-text-primary">FAQ</h3>
+                <p class="f16 ha-mb-0">Frequently Asked Questions</p>
+            </div>
+            <div class="ha-col ha-col-3 ha-align-right">
+                <a class="btn-more" href="">Get More FAQ ></a>
+            </div>
+            <div class="ha-col ha-col-12 ha-pt-3">
+                <p class="ha-mb-0">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad aut cum, facilis magnam mollitia nam
+                    nihil nobis nulla, pariatur porro, repudiandae unde. Architecto esse explicabo ipsum odit saepe,
+                    tenetur ut.</p>
             </div>
         </div>
         <div class="ha-row">
             <div class="ha-col ha-col-12">
-                <h3>Video Tutorial</h3>
-                <p>How to use Floating Effects and manage CSS Transform?</p>
-            </div>
-            <div class="ha-col ha-col-4">
-                <h4>How to create beautiful timeline with happyaddons in elementor</h4>
-            </div>
-            <div class="ha-col ha-col-4">
-                <h4>How to create beautiful timeline with happyaddons in elementor</h4>
-            </div>
-            <div class="ha-col ha-col-4">
-                <h4>How to create beautiful timeline with happyaddons in elementor</h4>
-            </div>
-            <div class="ha-col ha-col-12">
-                <a target="_blank" rel="noopener" href="">View more videos</a>
-            </div>
-        </div>
-        <div class="ha-row">
-            <div class="ha-col ha-col-6">
-                <h3>FAQ</h3>
-                <p>Frequently Asked Questions</p>
-            </div>
-            <div class="ha-col ha-col-6">
-                <a href="">Get More FAQ ></a>
-            </div>
-            <div class="ha-col ha-col-6">
-                <a href="">Get More FAQ ></a>
-            </div>
-            <div class="ha-col ha-col-6">
-                <a href="">Get More FAQ ></a>
-            </div>
-        </div>
-        <div class="ha-row">
-            <div class="ha-col ha-col-12">
-                <div class="ha-cta">
-                    <div class="ha-cta-content">
-                        <h3>Call for Contributors</h3>
-                        <p>you interested to contribute to making this plugin more awesome?</p>
-
-                        <a href="">How am I going to contribute?</a>
+                <div class="ha-cta ha-rounded">
+                <div class="ha-row ha-align-items-center">
+                    <div class="ha-col-8">
+                        <h3 class="ha-feature-title">Call for Contributors</h3>
+                        <p class="f16">Are you interested to contribute to making this plugin more awesome?</p>
+                        <a class="link" href="">How am I going to contribute?</a>
                     </div>
-                    <div class="ha-cta-action">
-                        <a href="">I like to contribute</a>
+                    <div class="ha-cta-action ha-col-4 ha-align-right">
+                        <a class="btn-contribute" href="">I like to contribute</a>
+                    </div>
+                </div>
+                </div>
+            </div>
+        </div>
+        <div class="ha-row ha-py-5">
+            <div class="ha-col ha-col-6">
+                <div class="ha-border-box ha-min-height-455">
+                    <img class="ha-img-fluid ha-title-icon-size" src="<?php echo HAPPY_ADDONS_ASSETS; ?>imgs/admin/support-call.svg" alt="">
+                    <h3 class="ha-feature-title ha-text-secondary">Support And Feedback</h3>
+                    <p class="f16">Feeling like to consult with an expert? Take live Chat support immediately from happpyaddons. We are always ready to help you 24/7.</p>
+                    <p class="f16 ha-mb-2"><strong>Or if you’re facing technical issues with our plugin, then please create a support ticket</strong></p>
+                    <a class="ha-btn ha-btn-secondary" target="_blank" rel="noopener" href="">View more videos</a>
+                </div>
+            </div>
+            <div class="ha-col ha-col-6">
+                <div class="ha-border-box ha-min-height-455">
+                    <img class="ha-img-fluid ha-title-icon-size" src="<?php echo HAPPY_ADDONS_ASSETS; ?>imgs/admin/newspaper.svg" alt="">
+                    <h3 class="ha-feature-title ha-text-primary">Newsletter Subscription</h3>
+                    <p class="f16">To get updated news, current offers, deals, tips please subscribe to our Newsletters.</p>
+                    <a class="ha-btn ha-btn-primary" target="_blank" rel="noopener" href="">Subscribe Now</a>
+                </div>
+            </div>
+        </div>
+
+        <div class="ha-row ha-py-5 ha-align-items-center">
+            <div class="ha-col ha-col-6">
+                <img class="ha-img-fluid" src="<?php echo HAPPY_ADDONS_ASSETS; ?>imgs/admin/art2.png" alt="">
+            </div>
+            <div class="ha-col ha-col-6">
+                <img class="ha-img-fluid ha-title-icon-size" src="<?php echo HAPPY_ADDONS_ASSETS; ?>imgs/admin/cross-game.svg" alt="">
+                <h3 class="ha-feature-title">Missing Any Feature?</h3>
+                <p class="f16">Are you in need of a feature that’s not available in our plugin? Feel free to do a
+                    feature request from here,</p>
+                <a class="ha-btn ha-btn-primary" target="_blank" rel="noopener" href="">Request Feature</a>
+            </div>
+        </div>
+
+        <div class="ha-row ha-py-5">
+            <div class="ha-col ha-col-12">
+                <div class="ha-border-box">
+                    <div class="ha-row ha-align-items-center">
+                        <div class="ha-col ha-col-3" >
+                            <img class="ha-img-fluid ha-pr-2" src="<?php echo HAPPY_ADDONS_ASSETS; ?>imgs/admin/c-icon.png" alt="">
+                        </div>
+                        <div class="ha-col ha-col-8">
+                            <h3 class="ha-feature-title ha-text-secondary ha-mt-0">Happy with Our Work?</h3>
+                            <p class="f16 ha-mb-2">We are really thankful to you that you have chosen our plugin. If our plugin brings a smile in your face while working, please share your happiness by giving us a 5***** rating in WordPress Org. It will make us happy and won’t take more than 2 mins.</p>
+                            <a class="ha-btn ha-btn-secondary" target="_blank" rel="noopener" href="">I’m Happy to Give You 5*</a>
+                        </div>
                     </div>
                 </div>
             </div>
-
         </div>
+
+        <div class="ha-row ha-pt-0 ha-pb-0">
+            <div class="ha-col ha-col-12">
+                <img class="ha-img-fluid ha-title-icon-size" src="<?php echo HAPPY_ADDONS_ASSETS; ?>imgs/admin/comment-square.svg" alt="">
+                <h3 class="ha-feature-title">Blog & Article</h3>
+                <p class="f16 ha-mb-0">Are you in need of a feature that’s not available in our plugin? Feel free to do a feature request from here,</p>
+            </div>
+        </div>
+
+        <div class="ha-row ha-pt-2 ha-pb-3">
+            <div class="ha-col ha-col-6">
+                <div class="ha-media">
+                    <a href=""><img src="<?php echo HAPPY_ADDONS_ASSETS; ?>imgs/admin/v-thumb.jpg" class="ha-align-self-center ha-mr-3 ha-rounded" alt="..."></a>
+                    <div class="ha-media-body">
+                        <h3 class="ha-mt-0-">
+                            <a href="">How to create beautiful
+                                timeline with happyaddons
+                                in elementor</a>
+                        </h3>
+                        <p class="ha-mb-0 ha-date">12 September 2019</p>
+                    </div>
+                </div>
+            </div>
+            <div class="ha-col ha-col-6">
+                <div class="ha-media">
+                    <a href=""><img src="<?php echo HAPPY_ADDONS_ASSETS; ?>imgs/admin/v-thumb.jpg" class="ha-align-self-center ha-mr-3 ha-rounded" alt="..."></a>
+                    <div class="ha-media-body">
+                        <h3 class="ha-mt-0-">
+                            <a href="">How to create beautiful
+                                timeline with happyaddons
+                                in elementor</a>
+                        </h3>
+                        <p class="ha-mb-0 ha-date">12 September 2019</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+
     </div>
 </div>
