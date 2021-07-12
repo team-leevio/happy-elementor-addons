f191913ff8b5e235a3f23292703bd2c730450555
sayedulsayem <sayedulsayem@gmail.com>
2021-07-12 14:47:51 +0600

    push to share code

--- extensions/advanced-tooltip.php
+++ extensions/advanced-tooltip.php
@@ -12,16 +12,25 @@ class Advanced_Tooltip {
     public static function init() {
         add_action('elementor/element/common/_section_style/after_section_end', [__CLASS__, 'add_controls_section'], 1);
 
-        add_action('elementor/widget/print_template', [__CLASS__, '_print_template'], 10, 2);
-        add_action('elementor/widget/before_render_content', [__CLASS__, 'before_section_render'], 10, 1);
-        add_action('elementor/widget/before_render_content', [__CLASS__, 'inject_markup_with_script']);
+        // add_action('elementor/widget/print_template', [__CLASS__, '_print_template'], 10, 2);
+        // add_action('elementor/widget/before_render_content', [__CLASS__, 'before_section_render'], 10, 1);
+        // add_action('elementor/widget/before_render_content', [__CLASS__, 'inject_markup_with_script']);
+
+        add_action( 'elementor/element/common/after_add_attributes', [__CLASS__, 'add_attributes'] );
     }
 
-    public function get_name() {
-        return 'ha-tooltip-section';
-    }
+    // public function get_name() {
+    //     return 'ha-advanced-section';
+    // }
 
-    // public static function add_controls_section( Element_Base $element) {
+    public static function add_attributes( Element_Base $element ) {
+		$settings = $element->get_settings_for_display();
+
+		if ( $settings['ha_advanced_tooltip_enable'] == 'yes' ) {
+			$element->add_render_attribute( '_wrapper', 'class', 'ha-advanced-tooltip' );
+		}
+	}
+
     public static function add_controls_section($element) {
 
         $element->start_controls_section(
@@ -47,51 +56,25 @@ class Advanced_Tooltip {
         $element->end_controls_section();
     }
 
-    // public static function before_section_render( Element_Base $element ) {
-    public static function before_section_render($element) {
-        // $advanced_tooltip = $element->get_settings_for_display( 'ha_advanced_tooltip_enable' );
-        $advanced_tooltip = $element->get_settings('ha_advanced_tooltip_enable');
+    // public static function before_section_render($element) {
+    //     $advanced_tooltip = $element->get_settings('ha_advanced_tooltip_enable');
 
-        if ($advanced_tooltip == 'yes') {
+    //     if ($advanced_tooltip == 'yes') {
 
-            $element->add_render_attribute(
-                '_wrapper',
-                [
-                    'id' => 'ha-advanced-tooltip-' . $element->get_id(),
-                    'class' => 'ha-advanced-tooltip'
-                ]
-            );
+    //         $element->add_render_attribute(
+    //             '_wrapper',
+    //             [
+    //                 'id' => 'ha-advanced-tooltip-' . $element->get_id(),
+    //                 'class' => 'ha-advanced-tooltip'
+    //             ]
+    //         );
 
-            wp_enqueue_style('tipso');
-            wp_enqueue_script('jquery-tipso');
-        }
-    }
+    //         wp_enqueue_style('tipso');
+    //         wp_enqueue_script('jquery-tipso');
+    //     }
+    // }
 
-    /**
-     * Render Particles Background output in the editor.
-     *
-     * Written as a Backbone JavaScript template and used to generate the live preview.
-     *
-     * @access public
-     * @param object $template for current template.
-     * @param object $widget for current widget.
-     */
-    public static function _print_template($template, $widget) { // phpcs:ignore PSR2.Methods.MethodDeclaration.Underscore
-        $old_template = $template;
-        ob_start(); ?>
-
-        <# if( 'yes'==settings.ha_advanced_tooltip_enable ) { view.addRenderAttribute( 'tooltip_data' , 'id' , 'ha-advanced-tooltip-' + view.getID() ); view.addRenderAttribute( 'tooltip_data' , 'class' , 'ha-advanced-tooltip' ); #>
-            <div {{{ view.getRenderAttributeString( 'tooltip_data' ) }}}></div>
-            <# } #>
-            
-        <?php
-        $slider_content = ob_get_contents();
-        ob_end_clean();
-        $template = $slider_content . $old_template;
-        return $template;
-    }
-
-    public static function inject_markup_with_script($element) {
+    public static function dhseioghieghioerhgoierghioergignergneri($element) {
         $settings = $element->get_settings_for_display();
         $data = $element->get_data();
 
