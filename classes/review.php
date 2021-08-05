a4cbecf6b707165718aaf42110f3581ac0d0e951
Sabbir Hasan <sabbirshouvo@gmail.com>
2021-08-05 18:16:13 +0600

    fix Image stack group

--- classes/review.php
+++ classes/review.php
@@ -21,20 +21,26 @@ class Review_Us {
     public static function ha_void_check_installation_time() {
 
         // Added Lines Start
-        $nobug = "";
-        $nobug = get_option( 'ha__spare_me' );
+        //$nobug = "";
+        $nobug = get_option( 'ha__spare_me', "0");
 
-        if ( ! $nobug ) {
-            $install_date = get_option( 'ha__plugin_activation_time' );
-            $past_date    = strtotime( '-10 days' );
+        // var_dump($nobug);
 
-            $remind_time = get_option( 'ha__remind_me' );
-            $remind_due  = strtotime( '+15 days', $remind_time );
-            $now         = strtotime( "now" );
+        if ($nobug == "1" || $nobug == "3") {
+            return;
+        }
 
-            if ( ( $past_date >= $install_date ) || ( $now >= $remind_due ) ) {
-                add_action( 'admin_notices', [__CLASS__, 'ha_void_grid_display_admin_notice']);
-            }
+        $install_date = get_option( 'ha__plugin_activation_time' );
+        $past_date    = strtotime( '-10 days' );
+
+        $remind_time = get_option( 'ha__remind_me' );
+        $remind_due  = strtotime( '+15 days', $remind_time );
+        $now         = strtotime( "now" );
+
+        if ($now >= $remind_due) {
+            add_action( 'admin_notices', [__CLASS__, 'ha_void_grid_display_admin_notice']);
+        }else if (($past_date >= $install_date) &&  $nobug !== "2") {
+            add_action( 'admin_notices', [__CLASS__, 'ha_void_grid_display_admin_notice']);
         }
     }
 
@@ -50,6 +56,7 @@ class Review_Us {
         if ( ! in_array( $pagenow, $exclude ) ) {
             $dont_disturb = esc_url( add_query_arg( 'spare_me', '1', self::ha_current_admin_url() ) );
             $remind_me    = esc_url( add_query_arg( 'remind_me', '1', self::ha_current_admin_url() ) );
+            $rated        = esc_url( add_query_arg( 'ha_rated', '1', self::ha_current_admin_url() ) );
             $reviewurl    = esc_url( 'https://wordpress.org/support/plugin/happy-elementor-addons/reviews/?filter=5' );
 
             printf( __( '<div class="notice ha-review-notice ha-review-notice--extended">
@@ -60,12 +67,13 @@ class Review_Us {
                     <h3>Enjoying Happy Addons?</h3>
                     <p>Thank you for choosing Happy Addons. If you have found our plugin useful and makes you smile, please consider giving us a 5-star rating on WordPress.org. It would mean the world to us.</p>
                     <div class="ha-review-notice__actions">
-                        <a href="%s" class="ha-review-button ha-review-button--cta"><span>Yes, You Deserve It!</span></a>
-                        <a href="%s" class="ha-review-button ha-review-button--cta ha-review-button--outline"><span>Remind Me Later</span></a>
-                        <a href="%s" class="ha-review-button ha-review-button--cta ha-review-button--error ha-review-button--outline"><span>No Thanks</span></a>
+                        <a href="%s" class="ha-review-button ha-review-button--cta"><span>👍 Yes, You Deserve It!</span></a>
+                        <a href="%s" class="ha-review-button ha-review-button--cta ha-review-button--outline"><span>🙌 Already Rated!</span></a>
+                        <a href="%s" class="ha-review-button ha-review-button--cta ha-review-button--outline"><span>🔔 Remind Me Later</span></a>
+                        <a href="%s" class="ha-review-button ha-review-button--cta ha-review-button--error ha-review-button--outline"><span>💔 No Thanks</span></a>
                     </div>
                 </div>
-            </div>' ), $reviewurl, $remind_me, $dont_disturb );
+            </div>' ), $reviewurl, $rated, $remind_me, $dont_disturb );
         }
     }
 
@@ -74,7 +82,7 @@ class Review_Us {
         if ( isset( $_GET['spare_me'] ) && ! empty( $_GET['spare_me'] ) ) {
             $spare_me = $_GET['spare_me'];
             if ( 1 == $spare_me ) {
-                add_option( 'ha__spare_me', TRUE );
+                update_option( 'ha__spare_me', "1" );
             }
         }
 
@@ -82,7 +90,16 @@ class Review_Us {
             $remind_me = $_GET['remind_me'];
             if ( 1 == $remind_me ) {
                 $get_activation_time = strtotime( "now" );
-                add_option( 'ha__remind_me', $get_activation_time );
+                update_option( 'ha__remind_me', $get_activation_time );
+                update_option( 'ha__spare_me', "2" );
+            }
+        }
+
+        if ( isset( $_GET['ha_rated'] ) && ! empty( $_GET['ha_rated'] ) ) {
+            $ha_rated = $_GET['ha_rated'];
+            if ( 1 == $ha_rated ) {
+                update_option( 'ha__rated', 'yes' );
+                update_option( 'ha__spare_me', "3" );
             }
         }
     }
