0a7d0ec23b720e5dad1bb885f08a49c96923d7e1
Kamal Hosen <kamalhosen8920@gmail.com>
2021-10-26 14:58:47 +0600

    3 widgets

---
+++ widgets/post-content/widget.php
@@ -0,0 +1,164 @@
+<?php
+/**
+ * Page_Title widget class
+ *
+ * @package Happy_Addons
+ */
+namespace Happy_Addons\Elementor\Widget;
+
+use Elementor\Core\Schemes\Typography;
+use Elementor\Utils;
+use Elementor\Control_Media;
+use Elementor\Controls_Manager;
+use Elementor\Group_Control_Border;
+use Elementor\Group_Control_Box_Shadow;
+use Elementor\Group_Control_Image_Size;
+use Elementor\Group_Control_Typography;
+use Elementor\Group_Control_Background;
+use Elementor\Group_Control_Text_Shadow;
+use Happy_Addons\Elementor\Controls\Group_Control_Foreground;
+
+defined( 'ABSPATH' ) || die();
+
+class Post_Content extends Base {
+
+	/**
+	 * Get widget title.
+	 *
+	 * @since 1.0.0
+	 * @access public
+	 *
+	 * @return string Widget title.
+	 */
+	public function get_title() {
+		return __( 'Post Content', 'happy-elementor-addons' );
+	}
+
+	public function get_custom_help_url() {
+		return 'https://happyaddons.com/docs/happy-addons-for-elementor/widgets/post-content/';
+	}
+
+	/**
+	 * Get widget icon.
+	 *
+	 * @since 1.0.0
+	 * @access public
+	 *
+	 * @return string Widget icon.
+	 */
+	public function get_icon() {
+		return 'hm hm-advanced-heading';
+	}
+
+	public function get_keywords() {
+		return [ 'content', 'text' ];
+	}
+
+	/**
+     * Register widget content controls
+     */
+	protected function register_content_controls() {
+
+		$this->start_controls_section(
+			'_section_post_content',
+			[
+				'label' => __( 'Post Content', 'happy-elementor-addons' ),
+				'tab' => Controls_Manager::TAB_CONTENT,
+			]
+		);
+
+        $this->add_responsive_control(
+			'align',
+			[
+				'label' => __( 'Alignment', 'happy-elementor-addons' ),
+				'type' => Controls_Manager::CHOOSE,
+				'options' => [
+					'left' => [
+						'title' => __( 'Left', 'happy-elementor-addons' ),
+						'icon' => 'eicon-text-align-left',
+					],
+					'center' => [
+						'title' => __( 'Center', 'happy-elementor-addons' ),
+						'icon' => 'eicon-text-align-center',
+					],
+					'right' => [
+						'title' => __( 'Right', 'happy-elementor-addons' ),
+						'icon' => 'eicon-text-align-right',
+					],
+					'justify' => [
+						'title' => __( 'Justify', 'happy-elementor-addons' ),
+						'icon' => 'eicon-text-align-justify',
+					],
+				],
+				'toggle' => true,
+				'selectors' => [
+					'{{WRAPPER}} .elementor-widget-container' => 'text-align: {{VALUE}};'
+				]
+			]
+		);
+
+        $this->end_controls_section();
+	}
+
+	/**
+	 * Register styles related controls
+	 */
+	protected function register_style_controls() {
+		$this->__page_title_style_controls();
+	}
+
+	
+	protected function __page_title_style_controls() {
+
+        $this->start_controls_section(
+            '_section_style_text',
+            [
+                'label' => __( 'Text', 'happy-elementor-addons' ),
+                'tab' => Controls_Manager::TAB_STYLE,
+            ]
+        );
+
+        $this->add_control(
+			'title_color',
+			[
+				'label' => esc_html__( 'Text Color', 'happy-elementor-addons' ),
+				'type' => Controls_Manager::COLOR,
+				'global' => [
+					// 'default' => Global_Colors::COLOR_PRIMARY,
+				],
+				'selectors' => [
+					'{{WRAPPER}} p' => 'color: {{VALUE}};',
+				],
+			]
+		);
+
+		$this->add_group_control(
+			Group_Control_Typography::get_type(),
+			[
+				'name' => 'typography',
+				'global' => [
+					// 'default' => Global_Typography::TYPOGRAPHY_PRIMARY,
+				],
+				'selector' => '{{WRAPPER}} p',
+			]
+		);
+        
+        $this->end_controls_section();
+	}
+
+	protected function render() {
+		static $have_posts = [];
+        $post = get_post();
+
+		$settings = $this->get_settings_for_display();
+
+		if ( post_password_required( $post->ID ) ) {
+			echo get_the_password_form( $post->ID );
+			return;
+		}
+
+		if ( isset( $have_posts[ $post->ID ] ) ) { return; }
+		$have_posts[ $post->ID ] = true;
+		echo apply_filters( 'the_content', get_the_content() );
+	}
+}
