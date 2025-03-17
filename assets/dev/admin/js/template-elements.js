5d6c097847a4738350eef2883dbd234815f8be03
Sazzad Hossian <sazzadtbd@gmail.com>
2025-03-17 11:49:55 +0600

    update Elementor V2 App Bar render function

--- assets/dev/admin/js/template-elements.js
+++ assets/dev/admin/js/template-elements.js
@@ -67,23 +67,20 @@
 	if ( typeof elementor !== "undefined" ) {
 		if ( window.elementorV2 && window.elementorV2.editorAppBar && window.elementorV2.editorAppBar.documentOptionsMenu ) {
 			let documentOptionsMenu = window.elementorV2.editorAppBar.documentOptionsMenu;
-
 			documentOptionsMenu.registerAction( {
 				id: "ha-template-condition",
 				group: [ "save" ],
 				priority: 10,
-				useProps: function () {
-					return {
-						icon: function () {
-							return window.React ? window.React.createElement( "i", { className: "hm hm-happyaddons" } ) : null;
-						},
-						title: window.wp ? window.wp.i18n.__( "Template Conditions", "happy-elementor-addons" ) : "Template Conditions",
-						visible: true,
-						onClick: function ( e ) {
-							e.preventDefault();
-							return elementor.trigger( "ha:templateCondition" );
-						}
-					};
+				props: {
+					icon: () => {
+						return React ? React.createElement( "i", { className: "hm hm-happyaddons" } ) : null;
+					},
+					title: window.wp ? window.wp.i18n.__( "Template Conditions", "happy-elementor-addons" ) : "Template Conditions",
+					visible: true,
+					onClick: ( e ) => {
+						e.preventDefault();
+						return elementor.trigger( "ha:templateCondition" );
+					}
 				}
 			} );
 
