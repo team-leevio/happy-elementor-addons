935f757f8c1c14ded98aaa52de4642317a2561a9
sazzadcse <sazzadtbd@gmail.com>
2026-04-05 14:45:14 +0600

    add popup & text highlight text feature

--- extensions/heading-text-animation.php
+++ extensions/heading-text-animation.php
@@ -199,7 +199,7 @@ class Heading_Text_Animation {
                     'reveal'       => __( 'Text Reveal', 'happy-elementor-addons' ),
                     'smoky_reveal' => __( 'Smoky Reveal', 'happy-elementor-addons' ),
                     'alt_reveal'   => __( 'Alternative Reveal', 'happy-elementor-addons' ),
-                    // 'popup_reveal'  => __( 'Popup Reveal', 'happy-elementor-addons' ),
+                    'popup_reveal'  => __( 'Popup Reveal', 'happy-elementor-addons' ),
                     // 'mixing_reveal' => __( 'Mixing Reveal', 'happy-elementor-addons' ),
                     'scale'        => __( 'Scale', 'happy-elementor-addons' ),
                     'text_flip'    => __( 'Text Flip', 'happy-elementor-addons' ),
